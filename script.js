791b2850fe4c8d5ceccef88e53462f3bb4a3f948
DosmaRina <dosmarina15@gmail.com>
2023-10-20 22:05:23 +0700

    search

---
+++ script.js
@@ -0,0 +1,29 @@
+const search = document.querySelector(".search-box input"),
+      images = document.querySelectorAll(".box");
+
+search.addEventListener("keyup", e =>{
+    if(e.key == "Enter"){
+        let searcValue = search.value,
+            value = searcValue.toLowerCase();
+            images.forEach(image =>{
+                if(value === image.dataset.name){
+                    return image.style.display = "block";
+                }
+                image.style.display = "none";
+         });
+    }
+});
+
+search.addEventListener("keyup", () =>{
+    if(search.value != "") return;
+
+    images.forEach(image =>{
+        image.style.display = "block";
+    })
+})
+
+
+
+
+
+// kata kunci pencarian = mario, doraemon, pikhacu, goku
